0053894316b8daf3749b72219ffd2ef044fcedfa
ItsJady-Taken <davidkhun2004@gmail.com>
2024-04-23 15:41:01 +0700

    completed exercise: 2/7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let repeat = "";
+    if(num < 0) {
+        return 'ERROR'
+    }
+    else {
+        for(let i = 0; i < num; i++) {
+        repeat += word;  
+    }
+    return repeat;
+    }
+    
 };
 
 // Do not edit below this line
